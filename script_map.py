46005fa6467fdf6b36b00c70f8c12904b7f52358
yoursshahnawaz <shahnawazgalaxy340@gmail.com>
2020-05-24 23:56:57 +0530

    Added all states & UTs | Filtered data

--- script_map.py
+++ script_map.py
@@ -30,13 +30,26 @@ for j in concatenated_range:
                covid_current['statewise'][j]['confirmed']])
     df_covid = pd.DataFrame(df, columns=['State', 'Total Case'])
 
-# Converting data to CSV 
+# More filtration to match index of both the files
+df2 = pd.DataFrame({'State':['Nagaland'],
+                  'Total Case':[0]})
+df_covid = pd.concat([df_covid, df2])
+df_covid = df_covid.sort_values('State', axis=0)
+df_covid = df_covid.reset_index(drop=True)
+
+# Converting Needed data to CSV 
+df_covid['State'].iloc[7] = 'Dadra and Nagar Haveli'
 df_covid.to_csv('Data/TotalCase.csv')
 
 # Reading CSV Data
 pop_df = pd.read_csv('Data/TotalCase.csv')
 
 map1 = flm.Map(location=[20.5937,78.9629], zoom_start=4)
+
+# Map Tiles (Different types of maps)
+tiles = ['stamenwatercolor', 'cartodbpositron', 'openstreetmap', 'stamenterrain']
+for tile in tiles:
+    flm.TileLayer(tile).add_to(map1)
 
 flm.Choropleth(
     geo_data=geojson_counties,
