dc622f608c23eab1ff6ecac16a93f94ef560edbe
yoursshahnawaz <shahnawazgalaxy340@gmail.com>
2020-05-26 13:26:29 +0530

    Display Info on mouse hover & data filter

--- script_map.py
+++ script_map.py
@@ -22,7 +22,7 @@ covid_current = res.json()
 df = []
 
 # To skip few entries (index 0 and index 15)
-concatenated_range = chain(range(1, 11), range(12, 36))
+concatenated_range = chain(range(1, 11), range(12, 35))
 
 # Filtering only required information
 for j in concatenated_range:
@@ -31,9 +31,12 @@ for j in concatenated_range:
     df_covid = pd.DataFrame(df, columns=['State', 'Total Case'])
 
 # More filtration to match index of both the files
-df2 = pd.DataFrame({'State':['Nagaland'],
-                  'Total Case':[0]})
+df2 = pd.DataFrame({'State':['Mizoram'],
+                  'Total Case':[1]})
+df3 = pd.DataFrame({'State':['Sikkim'],
+                    'Total Case':[1]})
 df_covid = pd.concat([df_covid, df2])
+df_covid = pd.concat([df_covid, df3])
 df_covid = df_covid.sort_values('State', axis=0)
 df_covid = df_covid.reset_index(drop=True)
 
@@ -44,6 +47,17 @@ df_covid.to_csv('Data/TotalCase.csv')
 # Reading CSV Data
 pop_df = pd.read_csv('Data/TotalCase.csv')
 
+# Adding total_cases to JSON file (Coordinates' file)
+for i in range(35):
+    if((geojson_counties['features'][i]['properties']['NAME_1']) == (df_covid['State'][i])):
+        geojson_counties['features'][i]['properties']['total_case'] = df_covid['Total Case'][i]
+
+# for i in range(33):
+#     if((geojson_counties['features'][i]['properties']['NAME_1']) == (df_covid['State'][i])):
+#         # geojson_counties['features'][i]['properties']['total_case'] = df_covid['Total Case'][i]
+#         print("Yes")
+
+# Choropleth map object
 map1 = flm.Map(location=[20.5937,78.9629], zoom_start=4)
 
 # Map Tiles (Different types of maps)
@@ -51,15 +65,22 @@ tiles = ['stamenwatercolor', 'cartodbpositron', 'openstreetmap', 'stamenterrain'
 for tile in tiles:
     flm.TileLayer(tile).add_to(map1)
 
-flm.Choropleth(
+choropleth = flm.Choropleth(
     geo_data=geojson_counties,
     name='Total Case',
     data=pop_df,
     columns=['State', 'Total Case'],
     key_on='feature.id',
-    fill_color='YlGn',
+    fill_color='Set2',
     fill_opacity=0.5,
-    line_opacity=0.5).add_to(map1)
+    line_opacity=0.5,
+    legend_name='State-wise COVID-19 Cases in INDIA',
+    ).add_to(map1)
+
+# Adding Hover Effect
+style_function = "font-size: 12px; font-weight: bold"
+choropleth.geojson.add_child(
+    flm.features.GeoJsonTooltip(fields=['NAME_1','total_case',], aliases=['State', 'Total Case'], lables=True, sticky=True, toLocaleString=True, style=style_function))
 
 # layer control to turn choropleth on or off
 flm.LayerControl().add_to(map1)
