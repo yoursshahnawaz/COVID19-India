5e4d39e9608748b57f2bcb712d4e85647ffc2509
yoursshahnawaz <shahnawazgalaxy340@gmail.com>
2020-06-17 13:19:09 +0530

    Removed unnecessary codes and some optimisation

--- script_map.py
+++ script_map.py
@@ -1,19 +1,14 @@
 import folium as flm
 from folium import plugins
-import ipywidgets
-import geocoder
-import geopy
-import numpy as np
 import pandas as pd
 import json
 import requests
-from itertools import chain
 
-# load geo_json
+# load geo_json data
 with open(r'Data/Indian_States.json') as f:
-    geojson_counties = json.load(f)
+    geojson_states = json.load(f)
 
-for i in geojson_counties['features']:
+for i in geojson_states['features']:
     i['id'] = i['properties']['NAME_1']
 
 # Loading Real-time data via API
@@ -21,29 +16,15 @@ res = requests.get('https://api.covid19india.org/data.json')
 covid_current = res.json()
 df = []
 
-# To skip few entries (index 0 and index 9)
-concatenated_range = chain(range(1, 8), range(9, 37))
-
 # Filtering only required information
-for j in concatenated_range:
-    df.append([covid_current['statewise'][j]['state'],
-               covid_current['statewise'][j]['confirmed'],
-               covid_current['statewise'][j]['active'],
-               covid_current['statewise'][j]['deaths'],
-               ])
-    df_covid = pd.DataFrame(df, columns=['State', 'Total Case', 'Active Case', 'Deaths'])
-
-# # Adding missing data
-# df2 = pd.DataFrame({'State':['Mizoram'],
-#                   'Total Case':[1],
-#                   'Active Case':[1],
-#                   'Deaths':[0]})
-# df3 = pd.DataFrame({'State':['Sikkim'],
-#                     'Total Case':[1],
-#                     'Active Case':[1],
-#                   'Deaths':[0]})
-# df_covid = pd.concat([df_covid, df2])
-# df_covid = pd.concat([df_covid, df3])
+for j in range(1, 37):
+    if(covid_current['statewise'][j]['state'] != 'State Unassigned'):
+        df.append([covid_current['statewise'][j]['state'],
+                covid_current['statewise'][j]['confirmed'],
+                covid_current['statewise'][j]['active'],
+                covid_current['statewise'][j]['deaths'],
+                ])
+        df_covid = pd.DataFrame(df, columns=['State', 'Total Case', 'Active Case', 'Deaths'])
 
 # More filtration to match index of both the files
 df_covid = df_covid.sort_values('State', axis=0)
@@ -58,10 +39,10 @@ pop_df = pd.read_csv('Data/TotalCase.csv')
 
 # Adding total_cases, active_cases, and total_deaths to JSON file (Coordinates' file)
 for i in range(35):
-    if((geojson_counties['features'][i]['properties']['NAME_1']) == (df_covid['State'][i])):
-        geojson_counties['features'][i]['properties']['total_case'] = df_covid['Total Case'][i]
-        geojson_counties['features'][i]['properties']['active_case'] = df_covid['Active Case'][i]
-        geojson_counties['features'][i]['properties']['total_deaths'] = df_covid['Deaths'][i]
+    if((geojson_states['features'][i]['properties']['NAME_1']) == (df_covid['State'][i])):
+        geojson_states['features'][i]['properties']['total_case'] = df_covid['Total Case'][i]
+        geojson_states['features'][i]['properties']['active_case'] = df_covid['Active Case'][i]
+        geojson_states['features'][i]['properties']['total_deaths'] = df_covid['Deaths'][i]
 
 # Choropleth map object
 map1 = flm.Map(location=[20.5937,78.9629], zoom_start=4, tiles='Mapbox Bright')
@@ -72,7 +53,7 @@ for tile in tiles:
     flm.TileLayer(tile).add_to(map1)
 
 choropleth = flm.Choropleth(
-    geo_data=geojson_counties,
+    geo_data=geojson_states,
     name='Total Case',
     data=pop_df,
     columns=['State', 'Total Case'],
@@ -96,5 +77,5 @@ choropleth.geojson.add_child(
 # layer control to turn choropleth on or off
 flm.LayerControl().add_to(map1)
 
-# display map
+# render map
 map1.save('IndianMap.html')
